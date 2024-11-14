225304d417cec2cb4d1347dc0b66c48240e77db5
Vaibhav Singh Tomar <vst17march@gmail.com>
2024-11-14 17:17:26 +0530

    link in the api.js has been updated for backend connection

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5050'});
+// const API = axios.create({baseURL: 'http://localhost:5050'});
+ const API = axios.create({baseURL: 'https://to-do-frontend-red.vercel.app/'});
 
 export const fetchTodos = () => API.get("/to-do");
 export const createTodo = (newTodo) => API.post("/to-do", newTodo);
-export const updateTodo = (id, updatedTodo) => API.patch(`/to-do/${id}`, updatedTodo);
-export const deleteTodo = (id) => API.delete(`/to-do/${id}`);
+export const updateTodo = (id, updatedTodo) => API.patch(`/to-do/${id}`
