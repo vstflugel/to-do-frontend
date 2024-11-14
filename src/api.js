a79d29ffcff68973a468f26a7fbb6b1687185476
Vaibhav Singh Tomar <vst17march@gmail.com>
2024-11-14 17:23:52 +0530

    link in the api.js has been updated for backend connection

--- src/api.js
+++ src/api.js
@@ -5,4 +5,5 @@ import axios from 'axios';
 
 export const fetchTodos = () => API.get("/to-do");
 export const createTodo = (newTodo) => API.post("/to-do", newTodo);
-export const updateTodo = (id, updatedTodo) => API.patch(`/to-do/${id}`
+export const updateTodo = (id, updatedTodo) => API.patch(`/to-do/${id}`, updatedTodo);
+export const deleteTodo = (id) => API.delete(`/to-do/${id}`);
